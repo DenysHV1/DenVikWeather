33b261d787477df5df3f861726ab8ddcdf103194
DenVik <denyagarkusha2@gmail.com>
2025-01-13 16:34:59 +0100

    add more svg

--- src/components/WeatherInfo/WeatherInfo.jsx
+++ src/components/WeatherInfo/WeatherInfo.jsx
@@ -8,7 +8,7 @@ import {
 } from "../../redux/selectors";
 import Loader from "../Loader/Loader";
 import { MdOutlineCloud } from "react-icons/md";
-import { LiaCloudRainSolid } from "react-icons/lia";
+import { LiaCloudRainSolid, LiaCloudSunRainSolid } from "react-icons/lia";
 import { BsCloudSnow } from "react-icons/bs";
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -16,6 +16,9 @@ import { Navigation, Mousewheel, Keyboard } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { FiSun } from "react-icons/fi";
+import { TiWeatherPartlySunny } from "react-icons/ti";
+import { useState } from "react";
 
 const WeatherInfo = () => {
   const info = useSelector(selectData);
@@ -23,17 +26,27 @@ const WeatherInfo = () => {
   const isLoading = useSelector(selectIsLoading);
   const cityName = useSelector(selectCityName);
 
-  const updatedCityName = `${cityName?.slice(0,1).toUpperCase()}${cityName?.slice(1, 20).toLowerCase()}`;
+  const updatedCityName = `${cityName.slice(0, 1).toUpperCase()}${cityName
+    .slice(1, 20)
+    .toLowerCase()}`;
 
-  const selectCloud = (rain, snowfall) => {
-    if (rain > snowfall && rain > 2.4) {
-      return <LiaCloudRainSolid className={s.cloud} />;
-    } else if (snowfall > rain && snowfall > 2.4) {
-      return <BsCloudSnow className={s.cloud} />;
-    } else {
-      return <MdOutlineCloud className={s.cloud} />;
-    }
-  };
+    const getCloudIcon = (rain, snowfall, cloud_cover, temp) => {
+      if (snowfall > 0.1) {
+        return <BsCloudSnow className={s.cloud} title="Snow" />;
+      } else if (rain > 1) {
+        return <LiaCloudRainSolid className={s.cloud} title="Rain" />;
+      } else if (cloud_cover > 70) {
+        return <MdOutlineCloud className={s.cloud} title="Cloudy" />;
+      } else if (cloud_cover > 30) {
+        return <TiWeatherPartlySunny className={s.cloud} title="Partly Cloudy" />;
+      } else if (temp > 30) {
+        return <FiSun className={s.sun} title="Sunny" />;
+      } else {
+        return <FiSun className={s.sun} title="Clear Sky" />;
+      }
+    };
+
+  console.log(info);
 
   const fixTime = (time) => {
     switch (Number(time)) {
@@ -56,7 +69,7 @@ const WeatherInfo = () => {
       case 48:
         return "00";
       default:
-        return "00"
+        return "00";
     }
   };
 
@@ -76,40 +89,48 @@ const WeatherInfo = () => {
             breakpoints={{
               320: {
                 slidesPerView: 1,
-                spaceBetween: 3,
+                spaceBetween: 1,
               },
               768: {
-                slidesPerView: 2,
-                spaceBetween: 3,
+                slidesPerView: 3,
+                spaceBetween: 1,
               },
               1024: {
-                slidesPerView: 3,
-                spaceBetween: 3,
+                slidesPerView: 4,
+                spaceBetween: 1,
               },
               1440: {
-                slidesPerView: 4,
-                spaceBetween: 3,
+                slidesPerView: 6,
+                spaceBetween: 1,
               },
             }}
           >
             {info?.length > 0 ? (
-              info.map(({ time, temp, rain, snowfall, speed }, idx) => (
-                <SwiperSlide key={`item${idx}`} className={s.listItem}>
-                  {selectCloud(rain, snowfall)}
-                  <p className={s.time}>{Number(time) > 23 ? `${fixTime(time)}:00`: `${time}:00`}</p>
-                  <p className={s.day}>{Number(time) > 23 ? "Tomorrow" : "Today"}</p>
-                  <div className={s.infoBlock1}>
-                    <p>Temperature: </p>
-                    <p className={s.info_item}>{`${temp} ℃`}</p>
-                  </div>
-                  <div className={s.infoBlock2}>
-                    <p>Wind speed: </p>
-                    <p className={s.info_item}>{`${((speed * 1000) / 3600)
-                      .toString()
-                      .slice(0, 3)} m/c`}</p>
-                  </div>
-                </SwiperSlide>
-              ))
+              info.map(
+                ({ time, temp, rain, snowfall, speed, cloud_cover }, idx) => (
+                  <SwiperSlide key={`item${idx}`} className={s.listItem}>
+                    <div className={s.icon}>
+                      {getCloudIcon(rain, snowfall, cloud_cover, temp)}
+                    </div>
+                    <p className={s.time}>
+                      {Number(time) > 23 ? `${fixTime(time)}:00` : `${time}:00`}
+                    </p>
+                    <p className={s.day}>
+                      {Number(time) > 23 ? "Tomorrow" : "Today"}
+                    </p>
+                    <div className={s.infoBlock1}>
+                      <p>Temperature: </p>
+                      <p className={s.info_item}>{`${temp} ℃`}</p>
+                    </div>
+                    <div className={s.infoBlock2}>
+                      <p>Wind speed: </p>
+                      <p className={s.info_item}>{`${((speed * 1000) / 3600)
+                        .toString()
+                        .slice(0, 3)} m/c`}</p>
+                    </div>
+                  </SwiperSlide>
+                )
+              )
             ) : (
               <li>Sorry we don't have info about this city!</li>
             )}
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -6,26 +6,32 @@ const BASE_URL = "https://api.open-meteo.com/v1";
 export const getWeatherByName = createAsyncThunk(
   "weather/getWeather",
   async (city, thunkAPI) => {
-
     try {
-        let coordinates = await axios.get(
-          `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`
-        );
-        let { latitude, longitude } = coordinates.data.results[0];
-  
+      let coordinates = await axios.get(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`
+      );
+      let { latitude, longitude } = coordinates.data.results[0];
+
       const params = {
         latitude: latitude,
         longitude: longitude,
-        hourly: "temperature_2m,rain,snowfall,cloud_cover_mid,cloud_cover_high,wind_speed_10m",
+        hourly: "temperature_2m,rain,snowfall,cloud_cover,wind_speed_10m",
         forecast_days: 2,
       };
       const response = await axios.get(`${BASE_URL}/forecast`, { params });
-      console.log(response.data);
-      
+
       let temperature = [];
-      const { temperature_2m, rain, snowfall, wind_speed_10m } = response.data.hourly;
+      const { temperature_2m, rain, snowfall, wind_speed_10m, cloud_cover } =
+        response.data.hourly;
       for (let i = 3; i <= 45; i += 3) {
-        temperature.push({ time: i, temp: temperature_2m[i], rein: rain[i], snowfall: snowfall[i], speed: wind_speed_10m[i] });
+        temperature.push({
+          time: i,
+          temp: temperature_2m[i],
+          rein: rain[i],
+          snowfall: snowfall[i],
+          speed: wind_speed_10m[i],
+          cloud_cover: cloud_cover[i],
+        });
       }
 
       return temperature;
@@ -34,26 +40,31 @@ export const getWeatherByName = createAsyncThunk(
     }
   }
 );
-
 
 export const getWeatherByCoordinates = createAsyncThunk(
   "weather/getWeatherByCoordinates",
   async (coordinates, thunkAPI) => {
-
     try {
       const params = {
         latitude: coordinates.latitude,
         longitude: coordinates.longitude,
-        hourly: "temperature_2m,rain,snowfall,cloud_cover_mid,cloud_cover_high,wind_speed_10m",
+        hourly: "temperature_2m,rain,snowfall,cloud_cover,wind_speed_10m",
         forecast_days: 2,
       };
       const response = await axios.get(`${BASE_URL}/forecast`, { params });
-      console.log(response.data);
-      
+
       let temperature = [];
-      const { temperature_2m, rain, snowfall, wind_speed_10m } = response.data.hourly;
+      const { temperature_2m, rain, snowfall, wind_speed_10m, cloud_cover } =
+        response.data.hourly;
       for (let i = 3; i <= 45; i += 3) {
-        temperature.push({ time: i, temp: temperature_2m[i], rein: rain[i], snowfall: snowfall[i], speed: wind_speed_10m[i] });
+        temperature.push({
+          time: i,
+          temp: temperature_2m[i],
+          rein: rain[i],
+          snowfall: snowfall[i],
+          speed: wind_speed_10m[i],
+          cloud_cover: cloud_cover[i],
+        });
       }
 
       return temperature;
@@ -62,5 +73,3 @@ export const getWeatherByCoordinates = createAsyncThunk(
     }
   }
 );
-
-
